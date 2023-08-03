35a03b7d5f02e7af4f7c899f48873ce41b7b3694
gayeon Kim <95395033+gayeonKim0123@users.noreply.github.com>
2023-08-03 13:10:52 +0900

    Add files via upload

---
+++ kiosk-api/src/main/java/kiosk/kioskApi/domain/PageData.java
@@ -0,0 +1,20 @@
+package kiosk.kioskApi.domain;
+
+import java.util.Map;
+
+public class PageData {
+    private Map<String, Double> data;
+
+    // Getters and setters
+    public Map<String, Double> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Double> data) {
+        this.data = data;
+    }
+}
+
+
+
+
---
+++ kiosk-api/src/main/java/kiosk/kioskApi/domain/PageDelayData.java
@@ -0,0 +1,16 @@
+package kiosk.kioskApi.domain;
+
+import java.util.Map;
+
+public class PageDelayData {
+    private Map<String, Integer> data;
+
+    // Getters and setters
+    public Map<String, Integer> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Integer> data) {
+        this.data = data;
+    }
+}
